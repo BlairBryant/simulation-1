20caf4ee9124f69e51feb1ccadef43c12f13282b
Blair <blairslc@gmail.com>
2018-02-16 17:16:36 -0700

    fixed stuff

--- src/Components/Bins.js
+++ src/Components/Bins.js
@@ -18,24 +18,21 @@ export default class Bins extends Component {
     // }
 
     componentDidMount() {
-        console.log(this.props.match)
         axios.get(`http://localhost:3500/api/bins/${this.props.match.params.id}`).then(res => {
-            console.log(res.data)
-            // this.setState({bins: res.data})
-            // console.log(this.state)
+            this.setState({bins: res.data})
         })
 
     }
 
     render() {
-        // console.log(this.state)
+        console.log(this.state)
         let mapped = this.state.bins.map((x, i) => {
-            return 
+            return (
             !this.state.bins[i].name ? 
             <Link to={`/api/bin/${this.props.match.params.id}${x.id}`} key={i}><button>+ Add inventory to bin</button></Link> 
             : 
             <Link to={`/api/bin/${this.props.match.params.id}${x.id}`} key={i}><button>{`Bin ${x.id}`}</button></Link>
-        })
+        )})
         return (
             <div>
                 <header className='binsHeader'>
